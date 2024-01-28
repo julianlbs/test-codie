fbb93ad32ba0e0bef12783c99fd51257505cc8ee
Julian Lima <julianlimalbs@gmail.com>
2024-01-28 17:49:35 -0300

    chore: mudança de cor base de texto para preto

--- components/styles/GlobalStyle.tsx
+++ components/styles/GlobalStyle.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    color: ${({ theme }) => theme.colors.primary};
+    color: black;
     padding: 0;
     margin: 0;
     font-family: "Inter", sans-serif;
