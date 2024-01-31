ef1aab4e1b8453bb758e5667dc939a5cad74b018
Julian Lima <julianlimalbs@gmail.com>
2024-01-30 23:08:26 -0300

    chore: move constantes para dentro de infra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import { theme } from "../data/theme";
+import { theme } from "@/infra/constants/theme";
 import { GlobalStyle } from "@/modules/styles";
 
 export default function App({ Component, pageProps }: AppProps) {
