aaf03bae5000d4e7ce31f6d4b7a02722b5b41365
Julian Lima <julianlimalbs@gmail.com>
2024-01-30 23:31:08 -0300

    chore: configuração do inversify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/infra/constants/theme";
 import { GlobalStyle } from "@/modules/styles";
+import "reflect-metadata";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
